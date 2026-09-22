f41abcdd7b1666fce84d04b9317eca278c6e2ddd
Paulo <pcrrp.pereira@gmail.com>
2026-09-21 22:38:50 -0300

    atualizando projeto com a develop

--- PI4_Senac-Paulo/Marketplace/mobile/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Ajuste a URL base conforme o seu ambiente.
-// Exemplo: se usar emulador de Android (10.0.2.2) ou o IP da sua máquina.
-const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-});
-
-export default api;
