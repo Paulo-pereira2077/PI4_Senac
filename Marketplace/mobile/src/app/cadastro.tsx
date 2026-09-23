33edd64e81bd0b9cef0a42ce3521dc7a5a5c7000
ADAYLSON ARCANJO EVARISTO <adaylson.aevaristo@senacsp.edu.br>
2026-09-22 21:51:01 -0300

    Login funcional

--- Marketplace/mobile/src/app/cadastro.tsx
+++ Marketplace/mobile/src/app/cadastro.tsx
@@ -8,26 +8,16 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
-<<<<<<< HEAD
-} from 'react-native';
-import { router } from 'expo-router'; // 1. Importando o router do Expo
-=======
   Alert,
 } from 'react-native';
-import { router } from 'expo-router';
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
+import { router } from 'expo-router'; // 1. Importando o router do Expo
+import realizarCadastro from '../services/authService';
 
 import CustomInput from '@/components/input';
 import PrimaryButton from '@/components/botao';
 import SocialButton from '@/components/botaoSocial';
-<<<<<<< HEAD
 import CustomCheckbox from '@/components/customCheckbox'; // Importando o novo Checkbox
 import { theme } from '@/temas';
-=======
-import CustomCheckbox from '@/components/customCheckbox';
-import { theme } from '@/temas';
-import api from '@/services/api';
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 
 const DividerWithText = ({ text }: { text: string }) => {
   return (
@@ -37,11 +27,8 @@ const DividerWithText = ({ text }: { text: string }) => {
       <View style={styles.dividerLine} />
     </View>
   );
-<<<<<<< HEAD
 }
-=======
-};
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
+
 
 export default function RegisterScreen() {
   const [username, setUsername] = useState('');
@@ -50,54 +37,29 @@ export default function RegisterScreen() {
   const [confirmEmail, setConfirmEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-<<<<<<< HEAD
   const [isSeller, setIsSeller] = useState(false); // Estado do checkbox
 
-  const handleRegister = () => {
-    console.log('Cadastro pressionado', { username, cpf, email, password, isSeller });
-    // router.replace('/'); // Vai para a home após cadastrar, limpando a pilha
-=======
-  const [isSeller, setIsSeller] = useState(false);
-
   const handleRegister = async () => {
-    try {
-      if (!username || !email || !confirmEmail || !password || !confirmPassword) {
-        Alert.alert('Erro', 'Preencha todos os campos obrigatórios.');
-        return;
-      }
-
-      if (email !== confirmEmail) {
-        Alert.alert('Erro', 'Os e-mails não coincidem.');
-        return;
-      }
-
-      if (password !== confirmPassword) {
-        Alert.alert('Erro', 'As senhas não coincidem.');
-        return;
-      }
-
-      const tipo_perfil = isSeller ? 'VENDEDOR' : 'CLIENTE';
-
-      const response = await api.post('/auth/register', {
-        nome: username,
-        email,
-        senha: password,
-        tipo_perfil,
-        cpf, 
-      });
-
-      console.log('Resposta cadastro:', response.data);
-
-      Alert.alert('Sucesso', 'Cadastro realizado com sucesso!');
-      router.replace('/login');
-    } catch (error: any) {
-      console.error('Erro no cadastro:', error?.response?.data || error.message);
-      Alert.alert(
-        'Erro',
-        error?.response?.data?.error || 'Não foi possível realizar o cadastro.'
-      );
+    if(!username || !cpf || !email || !password || !isSeller ){
+      Alert.alert('Aviso', 'Por favor, preencha todos os campos!');
+      return;
     }
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
+    
+    if( email  != confirmEmail || password != confirmPassword ){
+      Alert.alert('Aviso', 'Campos incorretos');
+      return;
+    }
+
+    try {
+      await realizarCadastro(username, email, password, isSeller==true?"Vendedor":"Cliente");
+
+      Alert.alert('Sucesso!', 'Sua conta foi criada.');
+
+      router.navigate('/login')
+
+    } catch (error) {
+      Alert.alert('Ops!', "Erro no servidor ");
+    } 
   };
 
   return (
@@ -107,15 +69,10 @@ export default function RegisterScreen() {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
         <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false}>
-<<<<<<< HEAD
           
           {/* Botão de voltar usando o router.back() */}
           <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
              <Text style={styles.backButtonText}>{"< Voltar"}</Text>
-=======
-          <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
-            <Text style={styles.backButtonText}>{'< Voltar'}</Text>
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
           </TouchableOpacity>
 
           <Text style={styles.title}>Criar Conta</Text>
@@ -139,11 +96,7 @@ export default function RegisterScreen() {
 
             <CustomInput
               label="Email"
-<<<<<<< HEAD
               placeholder="Entre com seu email ou numero de telefone"
-=======
-              placeholder="Entre com seu email"
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
               value={email}
               onChangeText={setEmail}
               keyboardType="email-address"
@@ -152,11 +105,7 @@ export default function RegisterScreen() {
 
             <CustomInput
               label="Confirmar Email"
-<<<<<<< HEAD
               placeholder="Entre com seu email ou numero de telefone"
-=======
-              placeholder="Confirme seu email"
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
               value={confirmEmail}
               onChangeText={setConfirmEmail}
               keyboardType="email-address"
@@ -173,17 +122,12 @@ export default function RegisterScreen() {
 
             <CustomInput
               label="Confirmar Senha"
-<<<<<<< HEAD
               placeholder="Entre com sua senha"
-=======
-              placeholder="Confirme sua senha"
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
               value={confirmPassword}
               onChangeText={setConfirmPassword}
               secureTextEntry
             />
 
-<<<<<<< HEAD
             {/* Novo componente de Checkbox */}
             <CustomCheckbox 
               label="Sou vendedor" 
@@ -196,16 +140,6 @@ export default function RegisterScreen() {
                 title="Cadastrar" 
                 onPress={handleRegister} 
               />
-=======
-            <CustomCheckbox
-              label="Sou vendedor"
-              value={isSeller}
-              onValueChange={setIsSeller}
-            />
-
-            <View style={styles.registerButtonWrapper}>
-              <PrimaryButton title="Cadastrar" onPress={handleRegister} />
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
             </View>
           </View>
 
@@ -216,10 +150,6 @@ export default function RegisterScreen() {
             <SocialButton iconName="G" onPress={() => console.log('Google')} />
             <SocialButton iconName="A" onPress={() => console.log('Apple')} />
           </View>
-<<<<<<< HEAD
-
-=======
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -227,7 +157,6 @@ export default function RegisterScreen() {
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
     safeArea: {
         flex: 1,
         backgroundColor: theme.colors.background,
@@ -280,58 +209,4 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginBottom: theme.spacing.xl, 
     },
-=======
-  safeArea: {
-    flex: 1,
-    backgroundColor: theme.colors.background,
-  },
-  flex1: {
-    flex: 1,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    paddingHorizontal: theme.spacing.l,
-    paddingTop: 20,
-    paddingBottom: theme.spacing.l,
-  },
-  backButton: {
-    marginBottom: theme.spacing.m,
-  },
-  backButtonText: {
-    color: theme.colors.textSecondary,
-    fontSize: theme.fonts.size.body,
-  },
-  title: {
-    fontSize: theme.fonts.size.title,
-    fontWeight: theme.fonts.weight.extraBold,
-    color: theme.colors.textPrimary,
-    marginBottom: theme.spacing.l,
-  },
-  formContainer: {
-    marginBottom: theme.spacing.l,
-  },
-  registerButtonWrapper: {
-    marginTop: theme.spacing.s,
-  },
-  dividerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: theme.spacing.l,
-  },
-  dividerLine: {
-    flex: 1,
-    height: 1,
-    backgroundColor: theme.colors.border,
-  },
-  dividerText: {
-    marginHorizontal: theme.spacing.m,
-    color: theme.colors.textPlaceholder,
-    fontSize: theme.fonts.size.small,
-  },
-  socialContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: theme.spacing.xl,
-  },
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 });
--- Marketplace/mobile/src/app/login.tsx
+++ Marketplace/mobile/src/app/login.tsx
@@ -1,9 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-=======
-import {useRouter} from 'expo-router';
-
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 import {
   SafeAreaView,
   View,
@@ -13,10 +8,6 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
-<<<<<<< HEAD
-=======
-  Alert,
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 } from 'react-native';
 
 import CustomInput from '@/components/input';
@@ -24,10 +15,6 @@ import PrimaryButton from '@/components/botao';
 import SocialButton from '@/components/botaoSocial';
 import { theme } from '@/temas';
 import { Link, router } from 'expo-router';
-<<<<<<< HEAD
-=======
-import api from '@/services/api';
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 
 const DividerWithText = ({ text }: { text: string }) => {
   return (
@@ -37,51 +24,13 @@ const DividerWithText = ({ text }: { text: string }) => {
       <View style={styles.dividerLine} />
     </View>
   );
-<<<<<<< HEAD
 }
-=======
-};
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-<<<<<<< HEAD
-=======
-  const router = useRouter();
 
-  const handleLogin = async () => {
-    try {
-      if (!email || !password) {
-        Alert.alert('Erro', 'Preencha e-mail e senha.');
-        return;
-      }
 
-      const response = await api.post('/auth/login', {
-        email,
-        senha: password,
-      });
-
-      console.log('Resposta login:', response.data);
-
-      const { nome, tipo_perfil } = response.data;
-
-      Alert.alert('Bem-vindo!', `Olá, ${nome}`);
-
-      if (tipo_perfil === 'VENDEDOR') {
-        router.replace('/homeAdm');
-      } else {
-        router.replace('/explore');
-      }
-    } catch (error: any) {
-      console.error('Erro no login:', error?.response?.data || error.message);
-      Alert.alert(
-        'Erro',
-        error?.response?.data?.error || 'E-mail ou senha incorretos.'
-      );
-    }
-  };
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -90,20 +39,13 @@ export default function LoginScreen() {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
         <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false}>
-<<<<<<< HEAD
           
-=======
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
           <Text style={styles.title}>Bem vindo de volta!</Text>
 
           <View style={styles.formContainer}>
             <CustomInput
               label="Email ou numero de telefone"
-<<<<<<< HEAD
               placeholder="Entre com seu email ou numero de telefone"
-=======
-              placeholder="Entre com seu email"
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
               value={email}
               onChangeText={setEmail}
               keyboardType="email-address"
@@ -115,11 +57,7 @@ export default function LoginScreen() {
               placeholder="Entre com sua senha"
               value={password}
               onChangeText={setPassword}
-<<<<<<< HEAD
               secureTextEntry // Oculta a senha
-=======
-              secureTextEntry
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
             />
 
             <TouchableOpacity style={styles.forgotPasswordButton}>
@@ -127,14 +65,10 @@ export default function LoginScreen() {
             </TouchableOpacity>
 
             <View style={styles.loginButtonWrapper}>
-<<<<<<< HEAD
               <PrimaryButton 
                 title="Login" 
                 onPress={() => router.navigate('/vendedor/anuncios')} 
               />
-=======
-              <PrimaryButton title="Login" onPress={handleLogin} />
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
             </View>
           </View>
 
@@ -148,25 +82,17 @@ export default function LoginScreen() {
 
           <View style={styles.footer}>
             <Text style={styles.footerText}>Não tem uma conta? </Text>
-<<<<<<< HEAD
             <TouchableOpacity onPress={() => console.log('Ir para Registro')}>
               <Link style={styles.footerLink} href="/cadastro">Registre agora</Link>
             </TouchableOpacity>
           </View>
 
-=======
-            <Link style={styles.footerLink} href="/cadastro">
-              Registre agora
-            </Link>
-          </View>
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
-<<<<<<< HEAD
 // ==========================================
 // 3. ESTILOS
 // ==========================================
@@ -245,77 +171,4 @@ const styles = StyleSheet.create({
         fontSize: theme.fonts.size.body, // 14
         fontWeight: theme.fonts.weight.semiBold, // 600
     },
-=======
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: theme.colors.background,
-  },
-  flex1: {
-    flex: 1,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    paddingHorizontal: theme.spacing.l,
-    paddingTop: 60,
-    paddingBottom: theme.spacing.l,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: theme.fonts.size.title,
-    fontWeight: theme.fonts.weight.extraBold,
-    color: theme.colors.textPrimary,
-    marginBottom: theme.spacing.xl,
-  },
-  formContainer: {
-    marginBottom: theme.spacing.l,
-  },
-  forgotPasswordButton: {
-    alignSelf: 'flex-end',
-    marginBottom: theme.spacing.l,
-  },
-  forgotPasswordText: {
-    color: theme.colors.primary,
-    fontSize: theme.fonts.size.body,
-    fontWeight: theme.fonts.weight.semiBold,
-  },
-  loginButtonWrapper: {
-    marginTop: theme.spacing.s,
-  },
-  dividerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: theme.spacing.l,
-  },
-  dividerLine: {
-    flex: 1,
-    height: 1,
-    backgroundColor: theme.colors.border,
-  },
-  dividerText: {
-    marginHorizontal: theme.spacing.m,
-    color: theme.colors.textPlaceholder,
-    fontSize: theme.fonts.size.small,
-  },
-  socialContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: theme.spacing.xl,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 'auto',
-    paddingTop: 20,
-  },
-  footerText: {
-    color: theme.colors.textSecondary,
-    fontSize: theme.fonts.size.body,
-  },
-  footerLink: {
-    color: theme.colors.primary,
-    fontSize: theme.fonts.size.body,
-    fontWeight: theme.fonts.weight.semiBold,
-  },
->>>>>>> 90012446721ba69795c9a13f7d23bb8afcfc02a8
 });
